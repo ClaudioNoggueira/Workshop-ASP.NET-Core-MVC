03671ba00864eff962d12ec5cb645e6e27e9cb2c
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-10 15:20:30 -0300

    Outras entidades e segunda migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +11,8 @@ namespace SalesWebMVC.Data {
             : base(options) {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
