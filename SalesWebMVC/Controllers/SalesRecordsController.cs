ab7c029ba018b257fe993f1be9b0d8aaaf4a6fc2
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-16 20:31:55 -0300

    Implementando busca agrupada

--- SalesWebMVC/Controllers/SalesRecordsController.cs
+++ SalesWebMVC/Controllers/SalesRecordsController.cs
@@ -30,8 +30,17 @@ namespace SalesWebMVC.Controllers {
             return View(result);
         }
 
-        public IActionResult GroupingSearch() {
-            return View();
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate) {
+            if (!minDate.HasValue) {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue) {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
