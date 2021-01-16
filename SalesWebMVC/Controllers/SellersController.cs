4fc70f275df6c902d70ca2712dda29c47eb8a72d
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-16 17:44:10 -0300

    Validação

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -35,6 +35,14 @@ namespace SalesWebMVC.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) {
+            if (!ModelState.IsValid) {
+                var departments = departmentService.FindAll();
+                var viewModel = new SellerFormViewModel {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
             sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -87,6 +95,14 @@ namespace SalesWebMVC.Controllers {
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller) {
+            if (!ModelState.IsValid) {
+                var departments = departmentService.FindAll();
+                var viewModel = new SellerFormViewModel {
+                    Seller = seller,
+                    Departments = departments
+                };
+                return View(viewModel);
+            }
             if (id != seller.Id) {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
